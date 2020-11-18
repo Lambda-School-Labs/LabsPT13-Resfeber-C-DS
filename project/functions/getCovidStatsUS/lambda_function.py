a4d11e35508d5af8490a36c9f8cd60c7f99d2185
Dan Anderson <danfanderson@gmail.com>
2020-11-18 13:39:27 -0600

    Update lambda_function.py

--- project/functions/getCovidStatsUS/lambda_function.py
+++ project/functions/getCovidStatsUS/lambda_function.py
@@ -2,30 +2,31 @@ import os
 import sys
 import json
 import psycopg2
+from psycopg2.extras import execute_values
 from sodapy import Socrata
-from datetime import date
+from datetime import datetime, date
 
 def lambda_handler(event, context):
     # Set up working variables
-    ret_dict = {'statusCode': 200, 'body': 'default message'}
+    ret_dict = {'statusCode': 200, 'body': json.dumps('default message')}
     
     # Grab environment variables
     MY_APP_TOKEN = os.getenv("COVID_API")
     if len(MY_APP_TOKEN) == 0:
         # error: missing API key
-        print("error: missing API key", file=sys.stderr)
+        print("ERROR: missing API key", file=sys.stderr)
         
         ret_dict['statusCode'] = 401
-        ret_dict['body'] = 'error: missing API key'
+        ret_dict['body'] = json.dumps('error: missing API key')
         return ret_dict
 
     PG_DB_PASSWORD = os.getenv("PG_DB_PASSWORD")
     if len(PG_DB_PASSWORD) == 0:
         # error: missing API key
-        print("error: missing Postgres database password", file=sys.stderr)
+        print("ERROR: missing Postgres database password", file=sys.stderr)
         
-        ret_dict['statusCode'] = 401
-        ret_dict['body'] = 'error: missing Postgres database password'
+        ret_dict['statusCode'] = 400
+        ret_dict['body'] = json.dumps('error: missing Postgres database password')
         return ret_dict
 
     # Configure a connection object
@@ -48,7 +49,9 @@ def lambda_handler(event, context):
 
     except (Exception, psycopg2.Error) as error :
         print (f"Error while connecting to PostgreSQL: {error}", file=sys.stderr)
-        return
+        ret_dict['statusCode'] = 500
+        ret_dict['body'] = json.dumps('error connecting to the Postgres database')
+        return ret_dict
 
     # Validate the date parameter (e.g. "2020-11-01")
     req_date = event["date"]
@@ -56,8 +59,8 @@ def lambda_handler(event, context):
         # error: invalid date parameter
         print("error: invalid date parameter", file=sys.stderr)
         
-        ret_dict['statusCode'] = 401
-        ret_dict['body'] = 'error: invalid date parameter'
+        ret_dict['statusCode'] = 400
+        ret_dict['body'] = json.dumps('error: invalid date parameter')
         return ret_dict
     
     # Construct a date/time string
@@ -67,13 +70,58 @@ def lambda_handler(event, context):
     client = Socrata('data.cdc.gov', MY_APP_TOKEN)
     # Fetch data from the API
     results = client.get("9mfq-cb36", submission_date=date_time)
+
+    # Construct an Insert SQL statement
+    sql = "INSERT INTO state_covid_stats (state, date, new_cases, time_created, covid_api_rsp, notes) " + \
+          "VALUES %s ON CONFLICT (state, date) DO NOTHING"
     
     # Iterate through the results
-    print(f"Number of elements in results: {len(results)}", file=sys.stderr)
+    print(f"INFO: Number data points returned by the CDC API: {len(results)}", file=sys.stderr)
+    vals_arr = []
     for rslt in results:
-        print(rslt, file=sys.stderr)
-    
-    return {
-        'statusCode': 200,
-        'body': json.dumps("my message to you")
-    }
+        tmp_tpl = (
+            rslt["state"],
+            req_date,
+            int(float(rslt["new_case"])),
+            datetime.now().strftime("%Y-%m-%dT%H:%M:%S"),
+            json.dumps(rslt),
+            ""
+        )
+
+        # append this "row" dictionary to our running array
+        vals_arr.append(tmp_tpl)
+
+    # Do we have any data (rows) to insert into the database?
+    if len(vals_arr) != 0:
+        # yes: attempt the database insert
+        try:
+            # stand up a db cursor
+            cursor = connection.cursor()
+            # execute the insert for multiple rows
+            execute_values(cursor, sql, vals_arr)
+            # commit the db changes
+            connection.commit()
+            # close the db connection
+            connection.close()
+
+        except (Exception, psycopg2.Error) as error :
+            print (f"ERROR: error occurred attempting to insert rows into the database: {error}", file=sys.stderr)
+            ret_dict['statusCode'] = 500
+            ret_dict['body'] = json.dumps('error connecting to the Postgres database')
+            return ret_dict
+
+    else:
+        # no data to insert
+            print (f"INFO: no Covid API data returned.  Nothing to insert into the database", file=sys.stderr)
+            ret_dict['statusCode'] = 204
+            ret_dict['body'] = json.dumps('no Covid API data returned; nothing to insert into the database')
+            return ret_dict
+
+    # Function completed processing
+    print (f"INFO: finished inserting: {len(vals_arr)} rows", file=sys.stderr)
+    ret_dict['statusCode'] = 200
+    ret_dict['body'] = json.dumps(f"INFO: finished inserting: {len(vals_arr)} rows")
+    return ret_dict
+
+# Test the function locally
+print(lambda_handler({"date": "2020-11-01"}, {}))
