8cd3a86c9870a6d48905eec36a0e260cafb3da6d
Dan Anderson <danfanderson@gmail.com>
2020-12-01 17:38:23 -0600

    Add Postgres connection support

--- project/app/api/predict.py
+++ project/app/api/predict.py
@@ -5,6 +5,8 @@ from fastapi import APIRouter
 import pandas as pd
 from pydantic import BaseModel, Field, validator
 
+import psycopg2
+
 import os 
 from dotenv import load_dotenv
 import pandas as pd
@@ -15,6 +17,42 @@ from datetime import timedelta
 
 log = logging.getLogger(__name__)
 router = APIRouter()
+
+#* Create a connection to the DS API Postgres database
+# Read database configuration from environment variables
+DS_DB_HOST      = os.getenv("DS_DB_HOST")
+DS_DB_PORT      = os.getenv("DS_DB_PORT")
+DS_DB_NAME      = os.getenv("DS_DB_NAME")
+DS_DB_USER      = os.getenv("DS_DB_USER")
+DS_DB_PASSWORD  = os.getenv("DS_DB_PASSWORD")
+
+# Missing database configuration?
+if len(DS_DB_HOST)      == 0     or      \
+    len(DS_DB_NAME)     == 0     or      \
+    len(DS_DB_USER)     == 0     or      \
+    len(DS_DB_PASSWORD) == 0:
+    log.error("One or more DB environment variables are missing.")
+
+# Configure a Postgres database connection object
+pg_conn = psycopg2.connect(user =        DS_DB_USER,
+                           password =    DS_DB_PASSWORD,
+                           host =        DS_DB_HOST,
+                           port =        DS_DB_PORT,
+                           database =    DS_DB_NAME)
+
+# Test the database connection
+try:
+    cursor = pg_conn.cursor()
+    # Print PostgreSQL Connection properties
+    log.info("Connecting to the database with these credentials: {conn_details}\n".format(conn_details=pg_conn.get_dsn_parameters()))
+
+    # Print PostgreSQL version
+    cursor.execute("SELECT version();")
+    record = cursor.fetchone()
+    log.info("Successfully connected to the database: {conn_details}\n".format(conn_details=record))
+
+except (Exception, psycopg2.Error) as error:
+    log.error("Error while connecting to PostgreSQL: {err}".format(err=error))
 
 
 class Item(BaseModel):
@@ -83,3 +121,4 @@ async def covid_by_state(state: dict):
     new= df[(df['state'] == state) & (df['submission_date'] > last_week)]
     new_cases= new['new_case'].astype('float').sum()
     return new_cases
+
