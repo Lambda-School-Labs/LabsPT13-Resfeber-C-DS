5e130d5f74ab401e650893efc53dcde33e2254e9
robinhester <robin.hester@yahoo.com>
2021-01-03 22:35:22 -0500

    setting up visualization for local and aws use

--- project/app/api/viz.py
+++ project/app/api/viz.py
@@ -29,11 +29,11 @@ async def viz(statecode: str):
     df3 = viz_readiness(STATE_POP, statecodes)
 
     fig = go.Figure(data=go.Choropleth(
-    locations=df3['index'], # Spatial coordinates
-    z = df3['covid_case_per_pop'].astype(float), # Data to be color-coded
-    locationmode = 'USA-states', # set of locations match entries in `locations`
-    colorscale = 'Reds',
-    colorbar_title = "Covid by Population",
+        locations=df3['index'], # Spatial coordinates
+        z = df3['covid_case_per_pop'].astype(float), # Data to be color-coded
+        locationmode = 'USA-states', # set of locations match entries in `locations`
+        colorscale = 'Reds',
+        colorbar_title = "Covid by Population",
     ))
 
     fig.update_layout(
@@ -43,32 +43,3 @@ async def viz(statecode: str):
 
     return fig.to_json()
 
-@router.get('/viz/covid_score')
-async def viz_covid(statecode: str):
-    """
-    Visualize covid score based off of covid cases reported in the last 14 days to the CDC
-    
-    ### Path Parameter
-    `statecode`: The [USPS 2 letter abbreviation](https://en.wikipedia.org/wiki/List_of_U.S._state_and_territory_abbreviations#Table) 
-    (case insensitive) for any of the 50 states or the District of Columbia.
-
-    ### Response
-    JSON string to render with [react-plotly.js](https://plotly.com/javascript/react/
-
-    """
-    df3 = viz_readiness_covid_score(STATE_POP)
-
-    fig = go.Figure(data=go.Choropleth(
-    locations=df3['index'], # Spatial coordinates
-    z = df3['covid_score'].astype(float), # Data to be color-coded
-    locationmode = 'USA-states', # set of locations match entries in `locations`
-    colorscale = 'Reds',
-    colorbar_title = "Covid Score: Statistical Difference in Covid Cases",
-    ))
-
-    fig.update_layout(
-        title_text = 'Covid Score by State',
-        geo_scope='usa', # limite map scope to USA
-    )
-
-    return fig.to_json()
